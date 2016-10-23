9b57c852280f0be6adde757e251e518b9435f2be
Rafael-Lopez-11 <lopez.rafael08@gmail.com>
2016-10-23 15:09:27 +0100

    Updated class headers.

--- TSP Algorithms/src/BruteForce.java
+++ TSP Algorithms/src/BruteForce.java
@@ -1,13 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Written by Rafael Lopez <lopez.rafael08@gmail.com>, 2016
  */
 
-/**
- *
- * @author Rafael
- */
 import java.util.*;
 
 public class BruteForce {
--- TSP Algorithms/src/NearestNeighbor.java
+++ TSP Algorithms/src/NearestNeighbor.java
@@ -2,15 +2,9 @@
 import java.util.HashMap;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Written by Rafael Lopez <lopez.rafael08@gmail.com>, 2016
  */
 
-/**
- *
- * @author Rafael Lopez
- */
 public class NearestNeighbor 
 {
     private static int[][] createSampleData()
